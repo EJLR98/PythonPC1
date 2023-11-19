4233088364fed54cc4133a9eebf2f5b4e20c6fa7
EJLR98 <150573350+EJLR98@users.noreply.github.com>
2023-11-19 00:18:01 +0000

    Subiendo ejercicio11

---
+++ Ejercicio 11.py
@@ -0,0 +1,8 @@
+# Lista original
+lista_original = [1, 1, 2, 3, 4, 4, 5, 1]
+
+# Eliminar duplicados usando un conjunto y convertir de nuevo a lista
+lista_procesada = list(set(lista_original))
+
+# Mostrar la lista procesada
+print(lista_procesada)
