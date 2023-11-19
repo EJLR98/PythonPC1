1adc3eac729074c9ac5b34425b6d41f1d9215c44
EJLR98 <150573350+EJLR98@users.noreply.github.com>
2023-11-19 00:08:57 +0000

    Subiendo ejercicio9

---
+++ Ejercicio 9.py
@@ -0,0 +1,9 @@
+# Definir la lista original
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+
+# Crear una copia invertida de la lista original
+lista_invertida = lista_original.copy()
+lista_invertida.reverse()
+
+# Mostrar la lista invertida
+print(lista_invertida)
